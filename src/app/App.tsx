f471cbef717a409f3e24124469df40bc0d321181
sv.konoplitskii <s.konoplitsky@yandex.com>
2025-10-26 18:12:45 +0300

    feat : add  shadcn

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/shared/ui/button';
+import './index.css';
+function App() {
+
+
+  return (
+    <>
+      <Button variant="secondary">Привет</Button>
+    </>
+  )
+}
+
+export default App
